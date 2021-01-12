140396add8dc625980a8789ac92698217d72aa3d
SjoerdVink <sjoerdvink@icloud.com>
2021-01-12 16:58:24 +0100

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import useStyles from './styles';
+import { ExpenseTrackerContext } from '../../context/context';
+import Form from './Form/Form';
+import List from './List/List';
+
+const Main = () => {
+    const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
+            <CardContent>
+                <Typography align='center' variant='h5'>Total Balance €{balance}</Typography> 
+                <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px'}}>
+                    Try saying: Add income of €100 in category salary for monday....
+                </Typography>
+                <Divider />
+                <Form />
+            </CardContent>
+            <CardContent className={classes.cardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <List />
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <SpeechProvider appId='e03b8857-88e3-4968-a7a3-798a4ea5c9e8' language='en-US'>
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>, 
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
