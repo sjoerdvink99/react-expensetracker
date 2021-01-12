05366b0c57694de8994f9a50e6271aa3020cdc3c
Sjoerd Vink <60132689+sjoerdvink99@users.noreply.github.com>
2021-01-12 17:03:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <SpeechProvider appId='e03b8857-88e3-4968-a7a3-798a4ea5c9e8' language='en-US'>
+    <SpeechProvider appId='#token' language='en-US'>
         <Provider>
             <App />
         </Provider>
